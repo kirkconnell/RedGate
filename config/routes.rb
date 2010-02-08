789302ce496b11f8b8e1a7d2874134c1e2381d42
kirkconnell <carlos.kirkconnell@gmail.com>
2010-02-08 16:43:48 -0600

    removing the 'gates' preffix from the URI to make activeresource configuration less annoying

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :measurements
   
-  map.gate 'gates/:gate_name', :controller => 'gates', :action => 'retrieve', 
+  map.queue ':gate_name', :controller => 'gates', :action => 'retrieve', 
               :conditions => {:method => :get}
            
-  map.dropbox 'gates/:gate_name.:format', :controller => 'gates', :action => 'receive',
+  map.dropbox ':gate_name.:format', :controller => 'gates', :action => 'receive',
               :conditions => {:method => :post}
 
 end
--- spec/controllers/routes_spec.rb
+++ spec/controllers/routes_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ApplicationController do
   it "should recognize gate URIs" do
-    gate_path("test_gate").should == "/gates/test_gate"
+      queue_path("test_gate").should == "/test_gate"
   end
 end
