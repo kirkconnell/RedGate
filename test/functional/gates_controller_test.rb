4ce6d025cad978c3dc07aa0af33099823f7e9c45
kirkconnell <carlos.kirkconnell@gmail.com>
2010-02-09 16:29:24 -0600

    removing old runit test

--- test/functional/gates_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GatesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "non existant gates should return 404" do
-    get :apples
-    assert_not_nil assigns(:error)
-  end
-end
