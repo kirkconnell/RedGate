ff4bddbca35cd72ec1eb50fd96ca5d420e39d7e9
kirkconnell <carlos.kirkconnell@gmail.com>
2010-02-09 11:56:47 -0600

    testing discard usage with sample gate

--- config/gates/invoice.rb
+++ config/gates/invoice.rb
@@ -1,5 +1,9 @@
 gate :invoices
 
+process "filter invalid prices" do |invoice|
+  discard if invoice.price < 0.01
+end
+
 process "calculate tax" do |invoice|
   invoice.tax = invoice.price * 0.008
   invoice.total = invoice.price + invoice.tax
